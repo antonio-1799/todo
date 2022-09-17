95dd0c2c8512ee40c58d876aa142f563a2b0bdf4
Antonio Aradaza <antonioaradaza9917@gmail.com>
2022-09-17 18:58:17 +0800

    feature: add indexes on specific todo columns

--- src/models/TodoModel.js
+++ src/models/TodoModel.js
@@ -27,6 +27,20 @@ const Todos = db.define('todos', {
     },
 },{
     underscored: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ['id']
+        },
+        {
+            unique: false,
+            fields: ['name']
+        },
+        {
+            unique: false,
+            fields: ['completed_at']
+        }
+    ],
     tableName: 'todos',
     modelName: 'Todos',
     timestamps: true
